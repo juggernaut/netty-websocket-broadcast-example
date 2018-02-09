e91e753bedd22b3fdca7d4c04afa9777f8007f0e
Ameya Lokare <lokare.ameya@gmail.com>
2018-02-09 00:21:27 -0800

    Introduce channel group

--- src/main/java/co/leanjava/pubsub/WebSocketMessageHandler.java
+++ src/main/java/co/leanjava/pubsub/WebSocketMessageHandler.java
@@ -2,8 +2,11 @@ package co.leanjava.pubsub;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +18,8 @@ public class WebSocketMessageHandler extends SimpleChannelInboundHandler<WebSock
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketMessageHandler.class);
 
+    private static final ChannelGroup allChannels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
         if (frame instanceof TextWebSocketFrame) {
@@ -22,5 +27,11 @@ public class WebSocketMessageHandler extends SimpleChannelInboundHandler<WebSock
         } else {
             throw new UnsupportedOperationException("Invalid websocket frame received");
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        LOGGER.info("Adding new channel {} to list of channels", ctx.channel().remoteAddress());
+        allChannels.add(ctx.channel());
     }
 }
